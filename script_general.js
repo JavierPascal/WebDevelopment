e50f294caae757161fc04155099a1de318205333
Pachada <dtrejog98@gmail.com>
2021-05-25 13:23:36 -0500

    Se agrego el modal

--- script_general.js
+++ script_general.js
@@ -1,11 +1,35 @@
+var email, username
+
+window.onload = function(){
+    email = localStorage.getItem("email")
+    username = localStorage.getItem("username")
+}
+
+
+function save_user(){
+    username = document.getElementById('username').value;
+    email = document.getElementById('email').value;
+    
+
+    if (username == "" || email == ""){
+        alert("all fields must be filled out");
+        return false;
+    }
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    location.href = "../Home_con_login/home_con_login.html"
+
+}
+
 /* Hide/show navbar on scroll */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar-sticky").style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("navbar-sticky").style.top = "-50px";
     }
     prevScrollpos = currentScrollPos;
 }
